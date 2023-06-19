d1b026a6530fb3a956ce086ac266cbe08cfa36f8
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-06-19 18:47:05 -0500

    fixed mobile nav staying open when window resized

--- CapStone/react_frontend/AWARE/src/components/Navbar.jsx
+++ CapStone/react_frontend/AWARE/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {CgProfile} from 'react-icons/cg'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
 
 function Navbar() {
   const [nav, setNav] = useState(false);
@@ -9,6 +10,24 @@ function Navbar() {
     setNav(!nav);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      console.log('Window was resized!');
+      
+      const windowWidth = window.innerWidth;
+      const breakpoint = 768;
+
+      if (windowWidth > breakpoint) {
+        setNav(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return() => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
@@ -24,12 +43,12 @@ function Navbar() {
         <li className='p-4'><CgProfile size={28}/></li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]' : 'fixed left-[-100%]'}>
+      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]' : 'fixed left-[-100%]'}>
         <div className='flex'>
-          <h1 className='w-full text-3xl font-bold text-white m-4'>AWARE</h1>
-          <CgProfile size={68} className='p-3'/>
+          <h1 className='w-full text-3xl font-bold text-white m-4 pt-3'>AWARE</h1>
+          <CgProfile size={50} className='m-4 pt-3'/>
         </div>
         <ul className='pt-12 uppercase'>
           <li className='p-4 border-b border-gray-600'>Home</li>
