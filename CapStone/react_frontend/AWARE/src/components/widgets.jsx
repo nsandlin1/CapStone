a98a5cbfdaea9aebedc5c31857ebbdc8708025c6
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-06-23 00:02:42 -0500

    widgets have been added

--- CapStone/react_frontend/AWARE/src/components/widgets.jsx
+++ CapStone/react_frontend/AWARE/src/components/widgets.jsx
@@ -1,24 +1,10 @@
 
 function Widget(props) {
 
-    const size = 'w-1/5';
     const className = `flex flex-col items-center bg-black rounded-xl overflow-auto ${props.width} ${props.height}`;
 
     return (
         <div className={className}>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
-            <h1 className="text-[#FFFFFF]">NEWS FEED</h1>
         </div>
     )
 }
--- CapStone/react_frontend/AWARE/src/pages/Homepage.jsx
+++ CapStone/react_frontend/AWARE/src/pages/Homepage.jsx
@@ -3,12 +3,17 @@ import Widget from "../components/widgets";
 function Homepage() {
     return (
         <div className="flex justify-center items-center h-[89vh] bg-slate-400">
-            <div className="flex justify-center w-[80%] h-[80vh] bg-orange-500">
-                <div className="flex flex-col p-2 gap-2 w-[60%]">
-                    <Widget width='w[100%] h-[40%]' />
-                    <Widget width='w[100%] h-[40%]' />
+            <div className="flex justify-center w-[100%] h-[80vh]">
+                <div className="flex flex-col w-[60%] h-[80vh] ">
+                    <div className="toptwo flex flex-row p-2 gap-2 w-[100%] h-[55%]">
+                        <Widget width='w-[50%]' height="h-[100%]"/>
+                        <Widget width='w-[50%]' height="h-[100%]"/>
+                    </div>
+                    <div className="leftone flex justify-center p-2 h-[45%]">
+                        <Widget width='w-[100%]' height='h-[100%]'/>
+                    </div>
                 </div>
-                <div className="w-[40%] p-2">
+                <div className="rightone w-[30%] h-[80vh] p-2">
                     <Widget width='w-[95%] md:w-[95%]' height='h-[100%]'/>
                 </div>
             </div>
