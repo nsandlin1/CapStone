8641ae9e8afb395b76487337bca8e7b59eda5155
Novi Sandlin <novi@h8n.com>
2023-06-27 18:09:20 -0500

    got basic return for bills

--- CapStone/backend/webapp/Blueprints/congress.py
+++ CapStone/backend/webapp/Blueprints/congress.py
@@ -4,11 +4,12 @@ from ..extensions import db
 from ..models import JpegUrl
 from ..schemas import jpeg_url_schema
 from loguru import logger
+from PyPDF2 import PdfReader
 
 congress = Blueprint('congress', __name__)
 
 @congress.route('/members')
-def memberdata():
+def members():
     # house or senate
     branch = request.args.get("branch")
     members = propublica_get_members(current_app.config["PROPUBLICA_API_KEY"], current_app.config["CURRENT_CONGRESS"], branch)
@@ -33,14 +34,14 @@ def memberdata():
     return members_chopped
 
 @congress.route('/member_image')
-def memberimage():
+def member_image():
     id = request.args.get("id")
     logger.debug(f"id: {id}")
     jpgUrl = db.session.get(JpegUrl, id)
 
     if jpgUrl == None:
         try:
-            image_url = get_image(current_app.config["CONGRESS_GOV_API_KEY"], id)
+            image_url = congressgov_get_image(current_app.config["CONGRESS_GOV_API_KEY"], id)
         except Exception as e:
             return str(e), 404
 
@@ -50,3 +51,32 @@ def memberimage():
         db.session.commit()
 
     return jpeg_url_schema.jsonify(jpgUrl)
+
+@congress.route('/get_bills')
+def get_bills():
+    bills = congressgov_get_bills(current_app.config["CONGRESS_GOV_API_KEY"])
+
+    # probably need to thread this it's slow af
+    refactored = []
+    for bill in bills:
+        print(bill["title"])
+        try:
+            content_url = congressgov_get_bill_contents(current_app.config["CONGRESS_GOV_API_KEY"], bill["congress"], bill["type"].lower(), bill["number"])
+            print("contents got")
+        except:
+            content_url = None
+
+        bill["content_url"] = content_url
+
+        refactored.append({
+                "title": bill["title"],
+                "originChamber": bill["originChamber"],
+                "number": bill["number"],
+                "updateDate": bill["updateDate"],
+                "content_url": bill["content_url"]
+        }) 
+
+    return refactored
+
+
+# GET /bill/{congress}/{billType}/{billNumber}/text for bill contents
