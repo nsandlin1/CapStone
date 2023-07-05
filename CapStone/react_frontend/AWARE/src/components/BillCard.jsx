307e758f1c276d67b2f0df0746ddc6bd7ac0a230
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-07-04 21:21:22 -0500

    however on them bills find out what happen

--- CapStone/react_frontend/AWARE/src/components/BillCard.jsx
+++ CapStone/react_frontend/AWARE/src/components/BillCard.jsx
@@ -7,7 +7,7 @@ export const BillCard = ({ onClass, bill }) => {
 
 
     return (
-        <div onClick={handleClick} className="flex flex-col cursor-pointer items-center justify-center text-center rounded-xl h-[30%] w-[98%] bg-slate-300">
+        <div onClick={handleClick} className="flex flex-col cursor-pointer transition hover:scale-95 items-center justify-center text-center rounded-xl h-[30%] w-[98%] bg-slate-300">
             {bill.title}
         </div>
     )
