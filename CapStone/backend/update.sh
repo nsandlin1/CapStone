d9d8682a3570ebca8b4e38ca26bdae3603027d90
Novi Sandlin <novi@h8n.com>
2023-07-19 10:34:48 -0500

    update fixed

--- CapStone/backend/update.sh
+++ CapStone/backend/update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -------------------------------
-# update bills db
-echo "Updating bills..."
-updated=$(curl -s http://127.0.0.1:5000/api/congress/get_bills?update=True)
-echo "Bills added:" $updated
+# # update bills db
+# echo "Updating bills..."
+# updated=$(curl -s http://127.0.0.1:5000/api/congress/get_bills?update=True)
+# echo "Bills added:" $updated
 echo -------------------------------
 # update state-level congressmen db
 # need for every state and branch, but jsut doing louisiana senate for now
@@ -17,25 +17,34 @@ states=(
     "SD" "TN" "TX" "UT" "VT" "VA" "WA" "WV" "WI" "WY"
 )
 echo "Updating State Congressmen..."
+# round=0
 updated=0
 for state in ${states[@]}; do
     new_state_senate=$(curl -s http://127.0.0.1:5000/api/congress/state_members?update=True\&state=$state\&branch=Senate)
+    sleep 60
     new_state_house=$(curl -s http://127.0.0.1:5000/api/congress/state_members?update=True\&state=$state\&branch=House)
+    sleep 60
+
     if [[ $new_state_senate == $no_more_reqs ]] || [[ $new_state_house == $no_more_reqs ]]
     then
         echo $no_more_reqs
         break
     fi
+
     updated=$(($updated + $new_state_senate))
     updated=$(($updated + $new_state_house))
-    # to avoid 10 requests per minutes cap
-    sleep 65
+    # round=$(($round + 1))
+    # sleep 2
+    # if [[ $(($round % 4)) == 0 ]]
+    # then
+    #     sleep 65
+    # fi
 done
 echo "State Congressmen added:" $updated
 echo -------------------------------
-echo "Updating Fed. Congressmen"
-updated=0
-updated=$(($updated + $(curl -s http://127.0.0.1:5000/api/congress/members?branch=senate\&update=True)))
-updated=$(($updated + $(curl -s http://localhost:5000/api/congress/members?branch=house\&update=True)))
-echo "Fed. Congressmen added:" $updated
+# echo "Updating Fed. Congressmen"
+# updated=0
+# updated=$(($updated + $(curl -s http://127.0.0.1:5000/api/congress/members?branch=senate\&update=True)))
+# updated=$(($updated + $(curl -s http://localhost:5000/api/congress/members?branch=house\&update=True)))
+# echo "Fed. Congressmen added:" $updated
 echo -------------------------------
