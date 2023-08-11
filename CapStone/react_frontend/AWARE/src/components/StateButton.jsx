a6912f82b4c97d9ab91e2b22c13b8a63b39d41f0
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-08-10 20:43:08 -0500

    added shadows... spooky

--- CapStone/react_frontend/AWARE/src/components/StateButton.jsx
+++ CapStone/react_frontend/AWARE/src/components/StateButton.jsx
@@ -1,6 +1,6 @@
 export const StateButton = ({ selected, onClick }) => {
     
-    const className = `flex items-center justify-center rounded-xl border border-gray-300 w-[30%] h-[85%] mt-auto lg:w-[9%] lg:h-[75%] md:w-[15%]
+    const className = `flex items-center shadow-md justify-center rounded-xl border border-gray-300 w-[30%] h-[85%] mt-auto lg:w-[9%] lg:h-[75%] md:w-[15%]
                         ${selected ? 'hover:bgdarkerblue bgblue text-white' : 'hover:bg-zinc-500 bg-zinc-300'}`
     
     return (
