d271bf235cca99fb7df3336e1e1897ab4db91453
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-09-13 15:50:48 -0500

    added to mock elections page

---
+++ CapStone/react_frontend/AWARE/src/components/TeacherElectionCard.jsx
@@ -0,0 +1,13 @@
+import { React, useState }from 'react';
+
+function MockElectionCard() {
+
+    return (
+        <div className='flex h-[20%] w-[45%] bg-yellow-400 m-4 rounded-xl items-center justify-center'>
+            This is a custom mock election
+        </div>
+    )
+
+}
+
+export default MockElectionCard;
--- CapStone/react_frontend/AWARE/src/pages/MockElections.jsx
+++ CapStone/react_frontend/AWARE/src/pages/MockElections.jsx
@@ -1,4 +1,6 @@
 import { React, useState }from 'react';
+import MockElectionCard from '../components/TeacherElectionCard';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 function MockElections(){
 
@@ -18,7 +20,7 @@ function MockElections(){
         <div className="flex flex-col justify-center items-center h-[91vh] w-[100%] pb-4 bg-slate-400">
             <div className='flex flex-row justify-left items-center h-[10%] w-[80%]'>
                 <div className='flex mt-4 w-[20%] h-[80%] rounded-xl'>
-                    <select className='rounded-xl w-[100%] text-xl text-center' 
+                    <select className='rounded-xl w-[100%] h-[90%] text-xl text-center' 
                             name="classes" 
                             id="classes"
                             onChange ={handleSelectChange}
@@ -34,10 +36,19 @@ function MockElections(){
                 </div>
             </div>
             <div className='flex items-center justify-center h-[90%] w-[100%] bg-slate-400'>
-                <div className='flex h-[95%] w-[80%] justify-center pt-4 bg-navy rounded-xl'>
-                    <h1 className='text-2xl text-white'>
-                        { selectedClass == "Null" ? "Please Select a Class to view available Ballots" : "Elections"}
-                    </h1>
+                <div className='flex flex-col h-[95%] w-[80%] justify-center items-center pt-4 bg-navy rounded-xl'>
+                    <div className='flex relative h-[10%] w-[100%] items-center justify-center'>
+                        < AiOutlinePlus size='48' className='absolute text-white top-1 right-10' />
+                        <h1 className='text-2xl text-white'>
+                            { selectedClass == "Null" ? "Please Select a Class to view available Ballots" : "Elections"}
+                        </h1>
+                    </div>
+                    { selectedClass == 'Null' ? <div className='flex flex-row h-[90%] w-[100%] justify-center'></div> : 
+                    <div className='flex flex-row h-[90%] w-[100%] justify-center'>
+                        <MockElectionCard />
+                        <MockElectionCard />
+                    </div>
+                    }
                 </div>
             </div>
         </div>
