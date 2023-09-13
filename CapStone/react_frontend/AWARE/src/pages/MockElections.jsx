3580feb049112989016c1732f564073607ddc255
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-09-13 18:15:35 -0500

    y do i care so much

--- CapStone/react_frontend/AWARE/src/pages/MockElections.jsx
+++ CapStone/react_frontend/AWARE/src/pages/MockElections.jsx
@@ -1,10 +1,30 @@
 import { React, useState }from 'react';
 import MockElectionCard from '../components/TeacherElectionCard';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { IoMdAdd } from 'react-icons/io';
+import { BiArrowBack } from 'react-icons/bi';
+import { useTransition, animated } from '@react-spring/web';
 
 function MockElections(){
 
     const [selectedClass, setSelectedClass] = useState("Null");
+    const [isVisible, setVisibile] = useState(false);
+    
+    const transitionCreateBallot = useTransition(isVisible, {
+        from: { transform: 'translateY(100%)', opacity: 0 },
+        enter: { transform: 'translateY(0)', opacity: 1},
+        leave: { transform: 'translateY(100%)', opacity: 0}
+    })
+
+    const transitionElection = useTransition(!isVisible, {
+        from: { transform: 'translateY(100%)', opacity: 0 },
+        enter: { transform: 'translateY(0)', opacity: 1},
+        leave: { transform: 'translateY(100%)', opacity: 0}
+        
+        // enter: { x: 0, y: 0, opacity: 1},
+        // leave: { x: 0, y: 800, opacity: 0}
+    })
+
+    
 
     const classData = [
         { value: 'Class1', name: 'Class 1' },
@@ -35,20 +55,33 @@ function MockElections(){
 
                 </div>
             </div>
-            <div className='flex items-center justify-center h-[90%] w-[100%] bg-slate-400'>
+            <div className='flex items-center justify-center h-[85%] w-[100%] bg-slate-400'>
                 <div className='flex flex-col h-[95%] w-[80%] justify-center items-center pt-4 bg-navy rounded-xl'>
-                    <div className='flex relative h-[10%] w-[100%] items-center justify-center'>
-                        < AiOutlinePlus size='48' className='absolute text-white top-1 right-10' />
-                        <h1 className='text-2xl text-white'>
-                            { selectedClass == "Null" ? "Please Select a Class to view available Ballots" : "Elections"}
-                        </h1>
-                    </div>
-                    { selectedClass == 'Null' ? <div className='flex flex-row h-[90%] w-[100%] justify-center'></div> : 
-                    <div className='flex flex-row h-[90%] w-[100%] justify-center'>
-                        <MockElectionCard />
-                        <MockElectionCard />
-                    </div>
-                    }
+                    { transitionElection((style, item) =>
+                        item &&
+                    <animated.div style={style} className='flex flex-col h-[100%] w-[100%]'>
+                        <div className='flex relative h-[10%] w-[100%] items-center justify-center'>
+                            { selectedClass == 'Null' ? 
+                                '' :
+                                < IoMdAdd size='48' className='absolute fill-white top-1 right-10' onClick= {() => {setVisibile( v => !v);}}/> 
+                            }
+                            <h1 className='text-3xl text-white'>
+                                { selectedClass == "Null" ? "Please Select a Class to view available Ballots" : "Elections"}
+                            </h1>
+                        </div>
+                        { selectedClass == 'Null' ? <div className='flex flex-row h-[90%] w-[100%] justify-center'></div> : 
+                        <div className='flex flex-row h-[90%] w-[100%] justify-center'>
+                            <MockElectionCard />
+                            <MockElectionCard />
+                        </div>
+                        }
+                    </animated.div>
+                    )}
+                    { transitionCreateBallot((style, item) =>
+                        item &&
+                    <animated.div style={style} className='flex relative h-[100%] w-[100%] bg-white rounded-xl'> 
+                        < BiArrowBack size='48' className='absolute fill-black top-1 left-10' onClick= {() => {setVisibile( v => !v);}}/> 
+                    </animated.div> )}
                 </div>
             </div>
         </div>
