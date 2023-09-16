7c9f4380eb302fd07642f2b7ee0fffcae59ac2c2
Novi Sandlin <novi@h8n.com>
2023-09-16 11:45:11 -0500

    database changes

---
+++ CapStone/backend/webapp/misc.py
@@ -0,0 +1,12 @@
+# for testing database insertion with foreign keys
+# @congress.route('/dbtest')
+# def dbtest():
+#     x = User("novi@h8n.com", "lola", "nsandlin1", "novi", "sandlin", "student")
+#     db.session.add(x)
+#     db.session.commit()
+
+#     t = Teacher(None, x.username)
+#     db.session.add(t)
+#     db.session.commit()
+
+#     return "yes"
