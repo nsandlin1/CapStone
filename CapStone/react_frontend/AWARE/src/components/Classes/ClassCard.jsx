68a2718a784935daadfc8b06bc4b91443d70cd96
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-09-19 17:43:25 -0500

    i hate web dev

--- CapStone/react_frontend/AWARE/src/components/Classes/ClassCard.jsx
+++ CapStone/react_frontend/AWARE/src/components/Classes/ClassCard.jsx
@@ -1,21 +1,40 @@
 import { FcCollapse } from "react-icons/fc";
+import { useState } from "react";
 
-export const ClassCard = () => {
+export const ClassCard = ({className, classTime, teacher, isExpanded, toggleCollapse}) => {
 
+
+    const cardContainer = `flex ${isExpanded ? 'h-[100%]' : 'h-[100%]'} w-[95%] rounded-xl bg-navy m-1`;
+    const basicInfo = `flex flex-row w-[100%] ${isExpanded ? 'h-[100%]' : 'h-[100%]'} items-center justify-center`;
+    const expandedInfo = `ExpandedInfo flex h-[85%] ${isExpanded ? '' : 'hidden'}`;
+
+    const rotate = isExpanded ? 'rotate(180deg)' : 'rotate(0)';
+    
     return (
-        <div className='flex h-[20%] w-[95%] rounded-xl bg-navy'>
-            <div className='flex w-[20%] justify-center items-center'>
-                <h1 className='text-3xl text-white whitespace-nowrap'>
-                    Intro to Civics
-                </h1>
-            </div>
-            <div className='flex w-[60%] justify-center items-center'>
-                <h1 className='text-3xl text-white whitespace-nowrap'>
-                    9:30 - 10:45
-                </h1>
-            </div>
-            <div className='flex w-[15%] items-center justify-end'>
-                < FcCollapse className='ClassesCollapseIcon' />
+        <div className={cardContainer}>
+            <div className='flex flex-col w-[100%] h-[100%] items-center justify-center'>
+                <div className={basicInfo}>
+                    <div className='flex w-[20%] h-[100%] justify-center items-center'>
+                        <h1 className='text-3xl text-white whitespace-nowrap'>
+                            {className}
+                        </h1>
+                    </div>
+                    <div className='flex w-[60%] justify-center items-center'>
+                        <h1 className='text-3xl text-white whitespace-nowrap'>
+                            {classTime}
+                        </h1>
+                    </div>
+                    <div className='flex w-[15%] items-center justify-end'>
+                        < FcCollapse 
+                            style={{ transform: rotate, transition: "all 0.2s linear" }} 
+                            className='ClassesCollapseIcon'  
+                            onClick={toggleCollapse}
+                        />
+                    </div>
+                </div>
+                <div className={expandedInfo}>
+
+                </div>
             </div>
         </div>
     )
--- CapStone/react_frontend/AWARE/src/pages/Classes.jsx
+++ CapStone/react_frontend/AWARE/src/pages/Classes.jsx
@@ -1,25 +1,84 @@
-import React from 'react';
+import {React, useState } from 'react';
 import { FcCollapse } from "react-icons/fc";
 import { ClassCard } from '../components/Classes/ClassCard';
 
 function Classes() {
 
+    const [expandedName, setExpandedName] = useState(null);
+    const [classes, setClasses] = useState([
+        {
+            className: 'Intro to Civics',
+            classTime: '9:30 - 10:45',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'AP Civics',
+            classTime: '11:00 - 12:15',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'Help',
+            classTime: '9:30 - 10:45',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'AP Civics',
+            classTime: '11:00 - 12:15',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'Intro to Civics',
+            classTime: '9:30 - 10:45',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'AP Civics',
+            classTime: '11:00 - 12:15',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'Intro to Civics',
+            classTime: '9:30 - 10:45',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'AP Civics',
+            classTime: '11:00 - 12:15',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'Intro to Civics',
+            classTime: '9:30 - 10:45',
+            teacher: 'Ronald Regan'
+        },
+        {
+            className: 'AP Civics',
+            classTime: '11:00 - 12:15',
+            teacher: 'Ronald Regan'
+        }
+    ])
+
+    const handleToggleCollapse = (name) => {
+        setExpandedName((prevName) => (prevName === name ? null : name))
+    };
+
+
     return (
         <div className='flex flex-col h-[91vh] w-[100%] bg-slate-400 justify-center items-center'>
-            <div className='flex flex-col h-[100%] w-[90%] rounded-xl items-center'>
+            <div className='flex flex-col h-[100%] w-[90%] rounded-xl items-center '>
                 <div className='flex flex-row h-[15%] w-[95%]'>
                     <div className='flex justify-center w-[15%]  items-center'>
-                        <h1 className='text-5xl text-white'>
+                        <h1 className='text-5xl text-white drop-shadow-lg'>
                             Classes
                         </h1>
                     </div>
                     <div className='w-[75%]'>
                     </div>
                     <div className='flex w-[10%] justify-center items-center'>
-                        <button className='rounded-xl w-[100%] h-[60%] bg-white' >Add Class</button>
+                        <button className='rounded-xl w-[100%] h-[60%] bg-white shadow-lg' >Add Class</button>
                     </div>
                 </div>
-                <div className='flex flex-col h-[85%] w-[95%] rounded-xl items-center justify-center py-2 bg-white mb-4 overflow-auto'>
+                <div className='flex flex-col h-[85%] w-[95%] rounded-xl items-center justify-center py-2 bg-white mb-4 shadow-lg overflow-auto'>
                     <div className='flex flex-row h-[10%] w-[95%] items-center'>
                         <div className='flex w-[20%] justify-center'>
                             <h1 className='text-3xl text-navy font-bold whitespace-nowrap '>
@@ -32,25 +91,22 @@ function Classes() {
                             </h1> 
                         </div>
                     </div>
-                    {/* <div className='flex h-[20%] w-[95%] rounded-xl bg-navy'>
-                        <div className='flex w-[20%] justify-center items-center'>
-                            <h1 className='text-3xl text-white whitespace-nowrap'>
-                                Intro to Civics
-                            </h1>
-                        </div>
-                        <div className='flex w-[60%] justify-center items-center'>
-                            <h1 className='text-3xl text-white whitespace-nowrap'>
-                                9:30 - 10:45
-                            </h1>
-                        </div>
-                        <div className='flex w-[15%] items-center justify-end'>
-                            < FcCollapse className='ClassesCollapseIcon' />
-                        </div>
-                    </div> */}
-                    < ClassCard />
-                    <div className='flex h-[70%]'>
-
+                    <div className='flex flex-col h-[90%] w-[100%] items-center'>
+                        {
+                            classes.map((clas) => (
+                                <div className='flex flex-col h-[100%] w-[100%] justify-center items-center'>
+                                    < ClassCard 
+                                        className={clas.className} 
+                                        classTime={clas.classTime}
+                                        teacher={clas.teacher}
+                                        isExpanded={expandedName === clas.className}
+                                        toggleCollapse={() => handleToggleCollapse(clas.className)}
+                                    />
+                                </div> 
+                            ))
+                        }
                     </div>
+                    {console.log(expandedName)}
                 </div>
             </div>
         </div>
