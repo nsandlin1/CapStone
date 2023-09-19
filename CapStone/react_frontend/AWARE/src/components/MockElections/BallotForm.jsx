79e33ef84bdcd7b18be001b877b80b47b4c3547e
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-09-18 19:09:22 -0500

    not having fun

---
+++ CapStone/react_frontend/AWARE/src/components/MockElections/BallotForm.jsx
@@ -0,0 +1,58 @@
+export const CandidateFrom = ({input}) => {
+
+    return (
+        <form className="flex justify-center bg-zinc-400 my-4 w-[100%] h-[100%] items-center rounded-xl">
+            <div className="flex flex-col w-[100%]">
+                <div className="flex mb-2 justify-center">
+                    <label className="text-xl text-white">Position: </label>
+                    <input
+                        className="w-[75%] h-[100%] ml-2 rounded-lg justify-center text-2xl"
+                        type='text'
+                        name='position'
+                    />
+                </div>
+                <div className="flex flex-row mt-1 justify-center">
+                    <label className="text-xl text-white pl-2">Name: </label>
+                    <input
+                        className="w-[40%] h-[100%] ml-1 rounded-lg justify-center text-2xl"
+                        type='text'
+                        name='position'
+                    />
+                    <label className="text-xl text-white pl-2">Party: </label>
+                <select
+                    className="w-[30%] h-[100%] ml-1 rounded-lg justify-center text-2xl"
+                    type='option'
+                    name='position'
+                />
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export const PolicyForm = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export const PolicyButton = ({onClick}) => {
+
+    return (
+        <button onClick={onClick} className='flex h-[100%] w-[30%] text-lg bg-white m-4 shadow-xl rounded-xl items-center justify-center'>
+            Add contests option for a policy
+        </button>
+    )
+}
+
+export const CandidateButton = ({onClick}) => {
+
+    return (
+        <button onClick={onClick} className='flex h-[100%] w-[30%] text-lg bg-white m-4 shadow-xl rounded-xl items-center justify-center'>
+            Add contests option for an office seat
+        </button>
+    )
+}
--- CapStone/react_frontend/AWARE/src/components/MockElections/CreateBallot.jsx
+++ CapStone/react_frontend/AWARE/src/components/MockElections/CreateBallot.jsx
@@ -1,9 +1,54 @@
+import { useState } from 'react';
 import { BiArrowBack } from 'react-icons/bi';
+import { PolicyButton, CandidateButton, CandidateFrom, PolicyForm } from './BallotForm';
 
 function CreateBallot({back}) {
+
+    const [selectedForms, setSelectedForms] = useState([]);
+
+    const handleAddForm = (formType) => {
+        // Create a new form object based on the selected type
+        const newForm = { type: formType, data: {} };
+        
+        // Add the form to the selectedForms array
+        setSelectedForms([...selectedForms, newForm]);
+      };
+
+    
+
     return (
-        <div className="w-[100%] h-[100%] bg-navy rounded-xl"> 
-            < BiArrowBack size='48' className='absolute fill-white top-1 left-10' onClick={back}/>
+        <div className="flex flex-col w-[100%] h-[100%] bg-navy rounded-xl"> 
+            <div className='flex flex-row w-[100%] h-[10%]'>
+                <div className='flex w-[70%]'>
+                    < BiArrowBack size='48' className='absolute fill-white top-1 left-10' onClick={back}/>
+                </div>
+                <div className='flex w-[30%] h-[100%] justify-end pr-10'>
+                    <button className='text-xl bg-white rounded-xl w-[30%]'>
+                        Save
+                    </button>
+                </div>
+            </div>
+            <div className='flex flex-col w-[100%] h-[90%] justify-center'>
+                <div className='flex flex-col w-[95%] h-[100%] items-center m-2 overflow-auto'>
+                        
+                    <div className='flex flex-col w-[90%] items-center rounded-xl'>
+                        {selectedForms.map((form, index) => (
+                            <div className='flex justify-center w-[80%] h-[100%] my-2'>
+                                {form.type === 'candidate' ? (
+                                <CandidateFrom input={form.data} />
+                                ) : (
+                                <PolicyForm data={form.data} />
+                                )}
+                                {/* <button onClick={() => handleRemoveForm(index)}>Remove Form</button> */}
+                            </div>
+                        ))}
+                        </div>
+                    <div className='flex flex-row w-[100%] mb-2 justify-center'>
+                        <PolicyButton onClick={() => handleAddForm('policy')}></PolicyButton>
+                        <CandidateButton onClick={() => handleAddForm('candidate')}></CandidateButton>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
