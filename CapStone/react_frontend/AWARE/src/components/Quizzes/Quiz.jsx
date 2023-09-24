e150fa070ac576e5a5676fa2780bc79eeacba263
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-09-24 18:48:56 -0500

    im tired

--- CapStone/react_frontend/AWARE/src/components/Quizzes/Quiz.jsx
+++ CapStone/react_frontend/AWARE/src/components/Quizzes/Quiz.jsx
@@ -1,26 +1,41 @@
 
+export const MultipleChoic = () => {
+
+}
+
+export const TF = () => {
+
+}
 
 export const Question = () => {
 
     return (
-        <div className='flex flex-col bg-yellow-400 w-full h-[100%] p-4 rounded-xl'>
-            <div className='flex flex-row'>
-                <label className="text-2xl w-[20%]"> Question: </label>
-                <input className="text-2xl rounded-xl w-[70%]">
-                </input>
+        <div className='flex flex-col bg-white w-full h-[100%] p-4 space-y-2 rounded-xl'>
+            <div className='flex flex-row w-full text-navy '>
+                <div className="flex w-[25%] font-bold">
+                    <label className="text-2xl w-full"> Question: </label>
+                </div>
+                <div className='flex w-[65%] justify-start'>
+                    <input className="text-2xl rounded-md pl-2 bg-white w-full border-2 border-navy">
+                    </input>
+                </div>
             </div>
-            <div>
-                <label className="text-xl pr-2 w-[20%]">
-                    Question Type:
-                </label>
-                <select
-                    className='rounded-md w-[70%]'
-                    type='option'
-                >
-                    <option>Pick One</option>
-                    <option>Multiple Choice</option>
-                    <option>True/False</option>
-                </select>
+            <div className="flex flex-row">
+                <div className="flex whitespace-nowrap w-[25%]">
+                    <label className="text-xl text-navy pr-2 w-full]">
+                        Question Type:
+                    </label>
+                </div>
+                <div className='flex w-[75%] justify-start'>
+                    <select
+                        className='rounded-md bg-white border-2 border-navy'
+                        type='option'
+                    >
+                        <option>Pick One</option>
+                        <option>Multiple Choice</option>
+                        <option>True/False</option>
+                    </select>
+                </div>
             </div>
         </div>
     )
@@ -36,3 +51,4 @@ export const AddQButton = ({onClick}) => {
     )
 
 }
+
