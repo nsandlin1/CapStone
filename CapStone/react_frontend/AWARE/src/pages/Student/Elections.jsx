2f63e2ad2b3a24bf57a17134485a83cd947f79b1
keatenluv <48927041+keatenluv@users.noreply.github.com>
2023-10-16 18:26:02 -0500

    basic student take elections page

--- CapStone/react_frontend/AWARE/src/pages/Student/Elections.jsx
+++ CapStone/react_frontend/AWARE/src/pages/Student/Elections.jsx
@@ -1,47 +1,64 @@
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import { StudentElectionCard } from "../../components/MockElections/StudentElectionCards";
+
+
 function Elections() {
+
+    const [elections, setElections] = useState([
+        {
+            id: 1,
+            class_election_title: 'Pencil Ban'
+        },
+        {
+            id: 2,
+            class_election_title: 'Presidential Election'
+        }
+    ]);
+
+
+    const availableQuizzesView = `${false ? 'hidden' : 'flex flex-col relative h-[85%] w-[95%] rounded-xl items-center justify-center bg-white mb-4 shadow-lg'}`
+
     return (
         <div className='flex flex-col h-[91vh] w-[100%] bg-slate-400 justify-center items-center'>
             <div className='flex flex-col h-[100%] w-[90%] rounded-xl items-center'>
                 <div className='flex flex-row h-[15%] w-[95%] items-center justify-center'>
                     <div className='text-3xl md:text-4xl lg:text-5xl text-white drop-shadow-lg'>
-                        {takingQuiz ? <h1>{currentQuizTitle}</h1> : 'Quizzes'}
+                        {true ? <h1>Ballots</h1> : 'Ballots'}
                     </div>
                 </div>
                 <div className={availableQuizzesView}>
                     <div className='flex flex-row h-[10%] w-[95%] items-center'>
                         <div className='flex w-[20%] justify-center'>
                             <h1 className='text-xl md:text-2xl lg:text-3xl text-navy font-bold whitespace-nowrap '>
-                                Quiz
+                                Election
                             </h1> 
                         </div>
                         <div className='flex w-[60%] justify-center items-center'>
                             <h1 className='text-xl md:text-2xl lg:text-3xl text-navy font-bold whitespace-nowrap '>
-                                Due Date
                             </h1> 
                         </div>
                         <div className='flex w-[20%] justify-center items-center'>
                             <h1 className='text-xl md:text-2xl lg:text-3xl text-navy font-bold whitespace-nowrap '>
-                                Grade
+                                Action
                             </h1> 
                         </div>
                     </div>
                     <div className='flex flex-col w-[100%] h-[90%] justify-start items-center overflow-auto'>
-                        {quizzes['error'] ? 
+                        {elections['error'] ? 
                             <h1 className="text-navy text-2xl md:text-6xl pt-24 font-bold text-wrap whitespace-normal">You have not registered for any class</h1>
                         :
-                        quizzes.map((quiz, idx) => (
-                                <QuizCard 
+                        elections.map((election, idx) => (
+                                <StudentElectionCard 
                                     key = {idx}
-                                    quizNum = {quiz.quizId}
-                                    quizTitle = {quiz.title}
-                                    quizDDate = {quiz.quizDDate}
-                                    quizGrade = {quiz.grade}
-                                    onTakeQuiz = {quiz.grade == null ? handleTakeQuiz : handleViewGrade}
+                                    ballotNum = {election.id}
+                                    electionTitle = {election.class_election_title}
+                                    voted = {false}
                                 />
                             ))}
                     </div>
                 </div>
-                {takingQuiz ? 
+                {/* {takingQuiz ? 
                     <div className="flex flex-col relative h-[85%] w-[95%] rounded-xl items-center justify-center bg-white mb-4 shadow-lg">
                         <TakeQuiz 
                             quizId = {currentQuiz}
@@ -51,7 +68,7 @@ function Elections() {
                     </div>
                 : 
                     ''
-                }
+                } */}
             </div>
             <ToastContainer  
                 position="top-center"
